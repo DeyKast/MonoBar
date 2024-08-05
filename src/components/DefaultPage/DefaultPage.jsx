b5cc05b886f88537ab95fe5f50222ce9e9ededa0
DeyKast <deykast@gmail.com>
2024-08-05 19:21:19 +0300

    try_2

--- src/components/DefaultPage/DefaultPage.jsx
+++ src/components/DefaultPage/DefaultPage.jsx
@@ -4,7 +4,7 @@ import css from './defaultPage.module.css';
 const DefaultPage = ({ title, children }) => {
   return (
     <div className={css.container}>
-      <div className={`section ${css.defaultMenuSection}`}>
+      <div className={`section ${css.defaultMenu}`}>
         <h1 className={css.defaultPageTitle}>{title}</h1>
         {children}
       </div>
